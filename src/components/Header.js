fdb44fe09e79f46590e47e6bf47b5c54a5f8650b
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-04-09 13:01:54 -0400

    creating header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 function Header() {
   return (
     <div class="jumbotron">
-      <h1 class="display-4">Hello, world!</h1>
-      <p class="lead">
+      <h1 class="display-4 text-center">Employee Directory</h1>
+      <p class="lead text-center">
         This is a simple hero unit, a simple jumbotron-style component for
         calling extra attention to featured content or information.
       </p>
