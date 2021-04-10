b411a204582f5476331025fd7ed44c9b9ef53ddd
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-04-10 10:36:22 -0400

    took away descrption, since i dont have that feature working yet lol

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,6 @@ function Header() {
       <p className="lead text-center">
         Type in search bar below for an employee's name.
       </p>
-      <p className="lead text-center">
-        Click the table header to toggle names in ascending/descending order.
-      </p>
     </div>
   );
 }
